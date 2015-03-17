f39b817bfbc9322d74eedbdaa1302d416388afed
Jason Hanggi <jason@tablexi.com>
2015-03-17 00:24:06 -0500

    Add ActiveAdmin load paths

--- break_it/lib/break_it/engine.rb
+++ break_it/lib/break_it/engine.rb
@@ -1,5 +1,11 @@
 module BreakIt
   class Engine < ::Rails::Engine
     isolate_namespace BreakIt
+
+    initializer :load_active_admin do
+      # Per https://github.com/activeadmin/activeadmin/wiki/Define-a-resource-inside-an-engine
+      # with a slight cleanup of building the path
+      ::ActiveAdmin.application.load_paths.unshift root.join('lib', 'admin')
+    end
   end
 end
